65a9d9457f64e357248d1a0452896cdf154dec0d
lummoxx <52716403+lummoxx@users.noreply.github.com>
2023-05-10 10:44:10 +0200

    Create d_deadCode_numberToExpressions.js

---
+++ d_deadCode_numberToExpressions.js
@@ -0,0 +1,68 @@
+var petronilla = tammitha;
+(function (dimitrious, spiros) {
+  var jaidn = tammitha;
+  var alisher = dimitrious();
+  while (true) {
+    try {
+      var xyair = parseInt(jaidn(210)) / 1 * (-parseInt(jaidn(215)) / 2) + -parseInt(jaidn(223)) / 3 + -parseInt(jaidn(208)) / 4 + parseInt(jaidn(220)) / 5 * (parseInt(jaidn(212)) / 6) + parseInt(jaidn(206)) / 7 + -parseInt(jaidn(205)) / 8 + parseInt(jaidn(218)) / 9 * (parseInt(jaidn(203)) / 10);
+      if (xyair === spiros) {
+        break;
+      } else {
+        alisher.push(alisher.shift());
+      }
+    } catch (tsuyuko) {
+      alisher.push(alisher.shift());
+    }
+  }
+}(josejuan, 979601));
+var a = 5;
+var b = 10;
+var c = petronilla(207);
+function tammitha(yasuo, antawn) {
+  var remilda = josejuan();
+  tammitha = function (gerika, abriyah) {
+    gerika = gerika - 203;
+    var lamarcus = remilda[gerika];
+    return lamarcus;
+  };
+  return tammitha(yasuo, antawn);
+}
+function greet(yadhir) {
+  var danetta = petronilla;
+  console.log(danetta(214) + yadhir + "!");
+}
+if (a > b) {
+  console[petronilla(209)](petronilla(222));
+} else if (a < b) {
+  console[petronilla(209)]("a is less than b");
+} else {
+  console[petronilla(209)](petronilla(216));
+}
+for (var i = 0; i < 10; i++) {
+  console[petronilla(209)](i);
+}
+console[petronilla(209)](3);
+var myObject = {name: petronilla(211), age: 30, city: petronilla(219)};
+console[petronilla(209)](myObject[petronilla(221)]);
+var x = 0;
+while (x < 5) {
+  console[petronilla(209)](x);
+  x++;
+}
+switch (c) {
+  case petronilla(207):
+    console[petronilla(209)](petronilla(213));
+    break;
+  case petronilla(204):
+    console[petronilla(209)](petronilla(217));
+    break;
+  default:
+    console[petronilla(209)]("c is neither a string nor a number");
+}
+function josejuan() {
+  var dennen = ["3199424nvnibb", "log", "1HoRPzC", "John", "2493102Knpayl", "c is a string", "Hello, ", "2756604vIRURx", "a is equal to b", "c is a number", "2506482PBkGAW", "New York", "20lXLieT", "name", "a is greater than b", "691086vjjikF", "110AlUvyq", "number", "12940248mEHGkM", "1960742aoWXaN", "string"];
+  josejuan = function () {
+    return dennen;
+  };
+  return josejuan();
+}

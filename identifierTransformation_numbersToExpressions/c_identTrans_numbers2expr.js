ecdde07504ba7ee6021b688aac431d4b46503d57
Aline <54989528+alinje@users.noreply.github.com>
2023-05-10 13:44:09 +0200

    Add files via upload

---
+++ identifierTransformation_numbersToExpressions/c_identTrans_numbers2expr.js
@@ -0,0 +1,70 @@
+// The provided code is obfuscated, but it can be deobfuscated as follows:
+
+// javascript
+
+var a = 4095;
+var b = 894;
+var c = 'string';
+
+function add(x, y) {
+    return x + y;
+}
+
+function multiply(x, y) {
+    return x * y;
+}
+
+function divide(x, y) {
+    return x / y;
+}
+
+function greet(name) {
+    console.log('Hello, ' + name + '!');
+}
+
+if (a > b) {
+    console.log('a is greater than b');
+} else if (a < b) {
+    console.log('a is less than b');
+} else {
+    console.log('a is equal to b');
+}
+
+for (var i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+var numbers = [
+    9657,
+    2205,
+    183,
+    -5773,
+    3142
+];
+console.log(numbers[3]);
+
+var person = {
+    name: 'John',
+    age: 30,
+    city: 'New York'
+};
+console.log(person.name);
+
+var counter = 0;
+while (counter < 10) {
+    console.log(counter);
+    counter++;
+}
+
+switch (c) {
+    case 'string':
+        console.log('c is a string');
+        break;
+    case 'number':
+        console.log('c is a number');
+        break;
+    default:
+        console.log('c is neither a string nor a number');
+}
+
+// This deobfuscated code is easier to read and understand. It defines variables, functions, conditional statements, loops, arrays, objects, and a switch case.

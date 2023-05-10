11bceaa38be8a22b40645e7ec518b93bb6053bf9
lummoxx <52716403+lummoxx@users.noreply.github.com>
2023-05-10 10:27:26 +0200

    Create controlFlow_IdentifierManipulation.js

---
+++ controlFlow_IdentifierManipulation.js
@@ -0,0 +1,77 @@
+var _0x55e00f = 0x5;
+var _0x4e5ce5 = 0xa;
+var _0x256bc0 = 'string';
+function _0x360f72(_0x3c92ef, _0x28b27b) {
+    var _0xa015f4 = {
+        'WqTnb': function (_0x50594c, _0x3d5321) {
+            return _0x50594c + _0x3d5321;
+        }
+    };
+    return _0xa015f4['WqTnb'](_0x3c92ef, _0x28b27b);
+}
+function _0x3c2acf(_0x40e597, _0x298cdf) {
+    var _0x56a654 = {
+        'zKCRd': function (_0x1c7ac9, _0x321750) {
+            return _0x1c7ac9 * _0x321750;
+        }
+    };
+    return _0x56a654['zKCRd'](_0x40e597, _0x298cdf);
+}
+function _0x47e4b3(_0x306edd, _0x43f8f9) {
+    var _0x41ddbc = {
+        'OTKHT': function (_0x179343, _0x8b2551) {
+            return _0x179343 / _0x8b2551;
+        }
+    };
+    return _0x41ddbc['OTKHT'](_0x306edd, _0x43f8f9);
+}
+function _0x45b7f2(_0x3444b4) {
+    var _0x97b033 = {
+        'IhOdw': function (_0x5c3d53, _0xca018a) {
+            return _0x5c3d53 + _0xca018a;
+        },
+        'Ilntp': function (_0x2c8d7f, _0x505c47) {
+            return _0x2c8d7f + _0x505c47;
+        }
+    };
+    console['log'](_0x97b033['IhOdw'](_0x97b033['Ilntp']('Hello,\x20', _0x3444b4), '!'));
+}
+if (_0x55e00f > _0x4e5ce5) {
+    console['log']('a\x20is\x20greater\x20than\x20b');
+} else if (_0x55e00f < _0x4e5ce5) {
+    console['log']('a\x20is\x20less\x20than\x20b');
+} else {
+    console['log']('a\x20is\x20equal\x20to\x20b');
+}
+for (var _0x5ee500 = 0x0; _0x5ee500 < 0xa; _0x5ee500++) {
+    console['log'](_0x5ee500);
+}
+var _0x1049be = [
+    0x1,
+    0x2,
+    0x3,
+    0x4,
+    0x5
+];
+console['log'](_0x1049be[0x2]);
+var _0x3e814c = {
+    'name': 'John',
+    '_0x44fe91': 0x1e,
+    'city': 'New\x20York'
+};
+console['log'](_0x3e814c['name']);
+var _0x5a6057 = 0x0;
+while (_0x5a6057 < 0x5) {
+    console['log'](_0x5a6057);
+    _0x5a6057++;
+}
+switch (_0x256bc0) {
+case 'string':
+    console['log']('c\x20is\x20a\x20string');
+    break;
+case 'number':
+    console['log']('c\x20is\x20a\x20number');
+    break;
+default:
+    console['log']('c\x20is\x20neither\x20a\x20string\x20nor\x20a\x20number');
+}

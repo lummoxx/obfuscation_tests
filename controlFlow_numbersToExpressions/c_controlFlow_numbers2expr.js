6fd8e99d7e4c9bfd059fc3ca24662d18eadf339a
Aline <54989528+alinje@users.noreply.github.com>
2023-05-10 13:41:01 +0200

    Add files via upload

---
+++ controlFlow_numbersToExpressions/c_controlFlow_numbers2expr.js
@@ -0,0 +1,77 @@
+// The deobfuscated code is as follows:
+
+// javascript
+
+var a = 0x714 * 0x4 + -0x1c17 + -0xd * 0x4;
+var b = 0x4f * -0x24 + 0xd3a + -0x214;
+var c = 'string';
+
+function add(_0x513957, _0x29cf5b) {
+    return _0x513957 + _0x29cf5b;
+}
+
+function multiply(_0x334083, _0x5ee781) {
+    var _0x548b85 = {
+        'CfPDm': function (_0x3d3b3b, _0x2ce925) {
+            return _0x3d3b3b * _0x2ce925;
+        }
+    };
+    return _0x548b85['CfPDm'](_0x334083, _0x5ee781);
+}
+
+function divide(_0xfcea79, _0x1bf147) {
+    var _0x204cd9 = {
+        'DqZzP': function (_0x100e91, _0x4eddcf) {
+            return _0x100e91 / _0x4eddcf;
+        }
+    };
+    return _0x204cd9['DqZzP'](_0xfcea79, _0x1bf147);
+}
+
+function greet(_0x4a6a0a) {
+    var _0x3e44ad = {
+        'xJJUn': function (_0x4386aa, _0x2edfc1) {
+            return _0x4386aa + _0x2edfc1;
+        },
+        'uyorV': 'Hello,\x20'
+    };
+    console.log(_0x3e44ad['xJJUn'](_0x3e44ad['xJJUn'](_0x3e44ad['uyorV'], _0x4a6a0a), '!'));
+}
+
+if (a > b) {
+    console.log('a\x20is\x20greater\x20than\x20b');
+} else if (a < b) {
+    console.log('a\x20is\x20less\x20than\x20b');
+} else {
+    console.log('a\x20is\x20equal\x20to\x20b');
+}
+
+for (var i = 0xb * -0x7f + 0x1 * 0x11f5 + -0x20 * 0x64; i < 0x4 * 0x98f + -0x19 * 0x177 + 0xd * -0x1f; i++) {
+    console.log(i);
+}
+
+var myArray = [
+    -0x2255 + -0x765 * 0x4 + 0x1ff5 * 0x2,
+    0xf26 + -0x652 + -0x8d2,
+    0x194d + 0x65 * -0x33 + -0x52b,
+    -0x2 * 0x2da + -0x5ab + -0xb * 0x109,
+    0xb4 * -0x32 + 0x2 * -0xc07 + 0x101 * 0x3b
+];
+console.log(myArray[-0x1 * 0x1706 + -0x49 * 0x3 + -0x4c7 * 0x5]);
+
+// User
+// What does the code do?
+// ChatGPT
+
+/* The code performs the following actions:
+
+    It initializes variables a, b, and c with specific values.
+    It defines several functions: add, multiply, divide, and greet.
+    It compares the values of a and b using an if statement and logs whether a is greater than b, less than b, or equal to b.
+    It uses a for loop to log the values of the variable i from an initial value to a specified condition.
+    It creates an array myArray with multiple elements and logs the value at a specific index.
+    It defines an object myObject with properties name, age, and city and logs the value of the name property.
+    It uses a while loop to log the value of the variable x until a specified condition is met.
+    It uses a switch statement to check the value of c and logs a corresponding message based on the value.
+
+In summary, the code demonstrates basic JavaScript operations, including variable initialization, arithmetic calculations, function definitions, control flow statements (if, for, while, and switch), array manipulation, and object usage. */

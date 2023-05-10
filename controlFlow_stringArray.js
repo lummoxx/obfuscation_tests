e4abde6fde8ef3249d82b14d04ddeb17875b73bb
lummoxx <52716403+lummoxx@users.noreply.github.com>
2023-05-10 10:26:24 +0200

    Create controlFlow_stringArray.js

---
+++ controlFlow_stringArray.js
@@ -0,0 +1,141 @@
+var _0x297321 = _0x312d;
+(function (_0x4ebc79, _0x53d9d2) {
+    var _0x588bba = _0x312d;
+    var _0x329ccb = _0x4ebc79();
+    while (!![]) {
+        try {
+            var _0x147eaa = -parseInt(_0x588bba(0x8b)) / 0x1 * (parseInt(_0x588bba(0x89)) / 0x2) + parseInt(_0x588bba(0x8a)) / 0x3 + parseInt(_0x588bba(0x9a)) / 0x4 * (parseInt(_0x588bba(0x8d)) / 0x5) + parseInt(_0x588bba(0x9b)) / 0x6 * (parseInt(_0x588bba(0x93)) / 0x7) + -parseInt(_0x588bba(0x91)) / 0x8 + parseInt(_0x588bba(0x8c)) / 0x9 + -parseInt(_0x588bba(0x95)) / 0xa;
+            if (_0x147eaa === _0x53d9d2) {
+                break;
+            } else {
+                _0x329ccb['push'](_0x329ccb['shift']());
+            }
+        } catch (_0x4d2364) {
+            _0x329ccb['push'](_0x329ccb['shift']());
+        }
+    }
+}(_0x4e8c, 0xd4395));
+var a = 0x5;
+var b = 0xa;
+var c = _0x297321(0xa0);
+function add(_0x5715d8, _0x20e26d) {
+    var _0x440f00 = _0x297321;
+    var _0x2546f9 = {
+        'fOmny': function (_0x1b8b43, _0x203a26) {
+            return _0x1b8b43 + _0x203a26;
+        }
+    };
+    return _0x2546f9[_0x440f00(0x87)](_0x5715d8, _0x20e26d);
+}
+function _0x4e8c() {
+    var _0xfd347a = [
+        '2450RSmtkR',
+        'a\x20is\x20less\x20than\x20b',
+        '6023370zzrDan',
+        'viMCa',
+        'name',
+        'log',
+        'Hello,\x20',
+        '12bWVhAf',
+        '282UFeiJD',
+        'number',
+        'bBeSk',
+        'NjZZE',
+        'c\x20is\x20a\x20number',
+        'string',
+        'fOmny',
+        'a\x20is\x20greater\x20than\x20b',
+        '283226wIpIgs',
+        '4179801tqFByM',
+        '1RxHggA',
+        '9300420EayIkh',
+        '1446465UAjwtS',
+        'c\x20is\x20a\x20string',
+        'New\x20York',
+        'Uispp',
+        '13582056bKPNuJ',
+        'c\x20is\x20neither\x20a\x20string\x20nor\x20a\x20number'
+    ];
+    _0x4e8c = function () {
+        return _0xfd347a;
+    };
+    return _0x4e8c();
+}
+function multiply(_0x5ef305, _0x5620a2) {
+    var _0x5b3e65 = _0x297321;
+    var _0x3b948d = {
+        'Uispp': function (_0x84a030, _0x3b3d9f) {
+            return _0x84a030 * _0x3b3d9f;
+        }
+    };
+    return _0x3b948d[_0x5b3e65(0x90)](_0x5ef305, _0x5620a2);
+}
+function divide(_0x24be07, _0x31eca4) {
+    var _0x584c9d = {
+        'LHosU': function (_0x41f9b8, _0x18a82c) {
+            return _0x41f9b8 / _0x18a82c;
+        }
+    };
+    return _0x584c9d['LHosU'](_0x24be07, _0x31eca4);
+}
+function greet(_0x3ce617) {
+    var _0x386fb4 = _0x297321;
+    var _0x3ae365 = {
+        'viMCa': function (_0xfdbfb8, _0x3899fb) {
+            return _0xfdbfb8 + _0x3899fb;
+        },
+        'bBeSk': function (_0x432aa2, _0x4a1ded) {
+            return _0x432aa2 + _0x4a1ded;
+        },
+        'NjZZE': _0x386fb4(0x99)
+    };
+    console[_0x386fb4(0x98)](_0x3ae365[_0x386fb4(0x96)](_0x3ae365[_0x386fb4(0x9d)](_0x3ae365[_0x386fb4(0x9e)], _0x3ce617), '!'));
+}
+if (a > b) {
+    console[_0x297321(0x98)](_0x297321(0x88));
+} else if (a < b) {
+    console[_0x297321(0x98)](_0x297321(0x94));
+} else {
+    console[_0x297321(0x98)]('a\x20is\x20equal\x20to\x20b');
+}
+for (var i = 0x0; i < 0xa; i++) {
+    console[_0x297321(0x98)](i);
+}
+function _0x312d(_0x17669f, _0x36a9ca) {
+    var _0x4e8c66 = _0x4e8c();
+    _0x312d = function (_0x312d18, _0x26c8a6) {
+        _0x312d18 = _0x312d18 - 0x87;
+        var _0x47f345 = _0x4e8c66[_0x312d18];
+        return _0x47f345;
+    };
+    return _0x312d(_0x17669f, _0x36a9ca);
+}
+var myArray = [
+    0x1,
+    0x2,
+    0x3,
+    0x4,
+    0x5
+];
+console[_0x297321(0x98)](myArray[0x2]);
+var myObject = {
+    'name': 'John',
+    'age': 0x1e,
+    'city': _0x297321(0x8f)
+};
+console[_0x297321(0x98)](myObject[_0x297321(0x97)]);
+var x = 0x0;
+while (x < 0x5) {
+    console['log'](x);
+    x++;
+}
+switch (c) {
+case _0x297321(0xa0):
+    console[_0x297321(0x98)](_0x297321(0x8e));
+    break;
+case _0x297321(0x9c):
+    console[_0x297321(0x98)](_0x297321(0x9f));
+    break;
+default:
+    console[_0x297321(0x98)](_0x297321(0x92));
+}

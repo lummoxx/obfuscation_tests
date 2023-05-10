f811f474d80122c157b97cda0aebe3ad5da7596a
Aline <54989528+alinje@users.noreply.github.com>
2023-05-10 13:41:47 +0200

    Add files via upload

---
+++ debugProtection_numbersToExpressions/c_debugProtec_numbers2expr.js
@@ -0,0 +1,161 @@
+// The deobfuscated code is as follows:
+
+// javascript
+
+var _0x503c26 = (function () {
+    var _0x2941b5 = true;
+    return function (_0x576a5b, _0xe89d7e) {
+        var _0x51dc74 = _0x2941b5 ? function () {
+            if (_0xe89d7e) {
+                var _0x376d62 = _0xe89d7e['apply'](_0x576a5b, arguments);
+                _0xe89d7e = null;
+                return _0x376d62;
+            }
+        } : function () {
+        };
+        _0x2941b5 = false;
+        return _0x51dc74;
+    };
+}());
+(function () {
+    _0x503c26(this, function () {
+        var _0x5c35fc = new RegExp('function\x20*\x5c(\x20*\x5c)');
+        var _0x557c69 = new RegExp('\x5c+\x5c+\x20*(?:[a-zA-Z_$][0-9a-zA-Z_$]*)', 'i');
+        var _0x5e5e95 = _0x1581d1('init');
+        if (!_0x5c35fc['test'](_0x5e5e95 + 'chain') || !_0x557c69['test'](_0x5e5e95 + 'input')) {
+            _0x5e5e95('0');
+        } else {
+            _0x1581d1();
+        }
+    })();
+}());
+var a = 0x18e1;
+var b = -0x11f7;
+var c = 'string';
+
+function add(_0xc3299, _0x43b627) {
+    return _0xc3299 + _0x43b627;
+}
+
+function multiply(_0x53d278, _0x1209e8) {
+    return _0x53d278 * _0x1209e8;
+}
+
+function divide(_0x4e0405, _0xa0fbaf) {
+    return _0x4e0405 / _0xa0fbaf;
+}
+
+function greet(_0x5e9d80) {
+    console.log('Hello, ' + _0x5e9d80 + '!');
+}
+
+if (a > b) {
+    console.log('a is greater than b');
+} else if (a < b) {
+    console.log('a is less than b');
+} else {
+    console.log('a is equal to b');
+}
+
+for (var i = -0x25eb; i < 0x82b; i++) {
+    console.log(i);
+}
+
+(function () {
+    var _0x78597c;
+    try {
+        var _0x15b603 = Function('return\x20(function()\x20' + '{}.constructor(\x22return\x20this\x22)(\x20)' + ');');
+        _0x78597c = _0x15b603();
+    } catch (_0x518282) {
+        _0x78597c = window;
+    }
+    _0x78597c['setInterval'](_0x1581d1, 0x6 * -0x570);
+}());
+
+var myArray = [
+    -0x1f8c,
+    -0x1c7d,
+    -0x35 * 0x122,
+    0x2377,
+    -0x245e
+];
+
+console.log(my
+
+// User
+// Can you finish the code?
+// ChatGPT
+
+// Certainly! Here's the complete code with the remaining parts:
+
+// javascript
+
+var myArray = [
+    -0x1f8c,
+    -0x1c7d,
+    -0x35 * 0x122,
+    0x2377,
+    -0x245e
+];
+
+console.log(myArray[0]);
+
+var myObject = {
+    'name': 'John',
+    'age': 0x1e,
+    'city': 'New York'
+};
+
+console.log(myObject['name']);
+
+var x = 0x1027;
+
+while (x < 0x5 * -0x45a) {
+    console.log(x);
+    x++;
+}
+
+var c = 'string';
+
+switch (c) {
+    case 'string':
+        console.log('c is a string');
+        break;
+    case 'number':
+        console.log('c is a number');
+        break;
+    default:
+        console.log('c is neither a string nor a number');
+}
+
+function _0x1581d1(_0x11cf21) {
+    function _0x5c224b(_0xfad03b) {
+        if (typeof _0xfad03b === 'string') {
+            return function (_0x4530af) {
+            }['constructor']('while\x20(true)\x20{}')['apply']('counter');
+        } else {
+            if (('' + _0xfad03b / _0xfad03b)['length'] !== -0xce5 || _0xfad03b % 0x20 === 0) {
+                (function () {
+                    return true;
+                }['constructor']('debu' + 'gger')['call']('action'));
+            } else {
+                (function () {
+                    return false;
+                }['constructor']('debu' + 'gger')['apply']('stateObject'));
+            }
+        }
+        _0x5c224b(++_0xfad03b);
+    }
+    try {
+        if (_0x11cf21) {
+            return _0x5c224b;
+        } else {
+            _0x5c224b(0x1);
+        }
+    } catch (_0x2a102e) {
+    }
+}
+
+_0x1581d1();
+
+// This code includes the missing parts and finishes the execution by calling the _0x1581d1() function at the end.

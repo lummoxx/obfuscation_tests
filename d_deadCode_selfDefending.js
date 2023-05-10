d2a21d6a7e27bdd8437315c9f2d397d27924be0c
lummoxx <52716403+lummoxx@users.noreply.github.com>
2023-05-10 10:45:08 +0200

    Create d_deadCode_selfDefending.js

---
+++ d_deadCode_selfDefending.js
@@ -0,0 +1,93 @@
+var yaphet = daria;
+(function (shabrie, jonny) {
+  var vionda = daria;
+  var evaan = shabrie();
+  while (true) {
+    try {
+      var jayelle = parseInt(vionda(264)) / 1 + -parseInt(vionda(272)) / 2 * (parseInt(vionda(266)) / 3) + -parseInt(vionda(259)) / 4 * (-parseInt(vionda(276)) / 5) + parseInt(vionda(260)) / 6 * (-parseInt(vionda(257)) / 7) + parseInt(vionda(261)) / 8 * (-parseInt(vionda(280)) / 9) + -parseInt(vionda(269)) / 10 + parseInt(vionda(265)) / 11;
+      if (jayelle === jonny) {
+        break;
+      } else {
+        evaan.push(evaan.shift());
+      }
+    } catch (maxie) {
+      evaan.push(evaan.shift());
+    }
+  }
+}(rollyn, 633240));
+var anjulie = function () {
+  var lutina = true;
+  return function (zeplynn, amilyn) {
+    var carpenter = lutina ? function () {
+      var shamyah = daria;
+      if (shamyah(267) === "XmNpJ") {
+        _0x497c29[shamyah(270)](_0x20aeaa);
+        _0xb65259++;
+      } else {
+        if (amilyn) {
+          var orvilla = amilyn[shamyah(271)](zeplynn, arguments);
+          amilyn = null;
+          return orvilla;
+        }
+      }
+    } : function () {};
+    lutina = false;
+    return carpenter;
+  };
+}();
+var veasna = anjulie(this, function () {
+  var kulsum = daria;
+  return veasna.toString()[kulsum(273)](kulsum(275)).toString().constructor(veasna)[kulsum(273)](kulsum(275));
+});
+veasna();
+var a = 5;
+function daria(amechi, melessa) {
+  var anyliah = rollyn();
+  daria = function (itohan, verlana) {
+    itohan = itohan - 256;
+    var lina = anyliah[itohan];
+    return lina;
+  };
+  return daria(amechi, melessa);
+}
+function rollyn() {
+  var ausie = ["apply", "994376DBzgrJ", "search", "string", "(((.+)+)+)+$", "1053210BLbuhU", "Hello, ", "c is a number", "John", "829179uKiQtd", "name", "7soXwhN", "c is a string", "20nyxTGK", "168156AFXmdq", "32dVEhcv", "a is less than b", "New York", "1005869HjXQna", "4834522WgOysn", "3QMAuST", "bpDPG", "a is equal to b", "9716030iSxtLD", "log"];
+  rollyn = function () {
+    return ausie;
+  };
+  return rollyn();
+}
+var b = 10;
+var c = yaphet(274);
+function greet(kathalia) {
+  var isamar = yaphet;
+  console[isamar(270)](isamar(277) + kathalia + "!");
+}
+if (a > b) {
+  console.log("a is greater than b");
+} else if (a < b) {
+  console[yaphet(270)](yaphet(262));
+} else {
+  console.log(yaphet(268));
+}
+for (var i = 0; i < 10; i++) {
+  console[yaphet(270)](i);
+}
+console[yaphet(270)](3);
+var myObject = {name: yaphet(279), age: 30, city: yaphet(263)};
+console[yaphet(270)](myObject[yaphet(256)]);
+var x = 0;
+while (x < 5) {
+  console.log(x);
+  x++;
+}
+switch (c) {
+  case yaphet(274):
+    console[yaphet(270)](yaphet(258));
+    break;
+  case "number":
+    console[yaphet(270)](yaphet(278));
+    break;
+  default:
+    console.log("c is neither a string nor a number");
+}

28f6f2c8bc49a62dffa183c853b58cfcbffd79f6
lummoxx <52716403+lummoxx@users.noreply.github.com>
2023-05-10 10:29:45 +0200

    Create controlFlow_numbersToExpressions.js

---
+++ controlFlow_numbersToExpressions.js
@@ -0,0 +1,70 @@
+var a = 0x714 * 0x4 + -0x1c17 + -0xd * 0x4;
+var b = 0x4f * -0x24 + 0xd3a + -0x214;
+var c = 'string';
+function add(_0x513957, _0x29cf5b) {
+    return _0x513957 + _0x29cf5b;
+}
+function multiply(_0x334083, _0x5ee781) {
+    var _0x548b85 = {
+        'CfPDm': function (_0x3d3b3b, _0x2ce925) {
+            return _0x3d3b3b * _0x2ce925;
+        }
+    };
+    return _0x548b85['CfPDm'](_0x334083, _0x5ee781);
+}
+function divide(_0xfcea79, _0x1bf147) {
+    var _0x204cd9 = {
+        'DqZzP': function (_0x100e91, _0x4eddcf) {
+            return _0x100e91 / _0x4eddcf;
+        }
+    };
+    return _0x204cd9['DqZzP'](_0xfcea79, _0x1bf147);
+}
+function greet(_0x4a6a0a) {
+    var _0x3e44ad = {
+        'xJJUn': function (_0x4386aa, _0x2edfc1) {
+            return _0x4386aa + _0x2edfc1;
+        },
+        'uyorV': 'Hello,\x20'
+    };
+    console['log'](_0x3e44ad['xJJUn'](_0x3e44ad['xJJUn'](_0x3e44ad['uyorV'], _0x4a6a0a), '!'));
+}
+if (a > b) {
+    console['log']('a\x20is\x20greater\x20than\x20b');
+} else if (a < b) {
+    console['log']('a\x20is\x20less\x20than\x20b');
+} else {
+    console['log']('a\x20is\x20equal\x20to\x20b');
+}
+for (var i = 0xb * -0x7f + 0x1 * 0x11f5 + -0x20 * 0x64; i < 0x4 * 0x98f + -0x19 * 0x177 + 0xd * -0x1f; i++) {
+    console['log'](i);
+}
+var myArray = [
+    -0x2255 + -0x765 * 0x4 + 0x1ff5 * 0x2,
+    0xf26 + -0x652 + -0x8d2,
+    0x194d + 0x65 * -0x33 + -0x52b,
+    -0x2 * 0x2da + -0x5ab + -0xb * -0x109,
+    0xb4 * -0x32 + 0x2 * -0xc07 + 0x101 * 0x3b
+];
+console['log'](myArray[-0x1 * 0x1706 + -0x49 * 0x3 + -0x4c7 * -0x5]);
+var myObject = {
+    'name': 'John',
+    'age': 0x1e,
+    'city': 'New\x20York'
+};
+console['log'](myObject['name']);
+var x = 0x127 * -0x5 + -0x206b + 0x262e;
+while (x < 0x93 * 0x13 + -0xcc + -0x286 * 0x4) {
+    console['log'](x);
+    x++;
+}
+switch (c) {
+case 'string':
+    console['log']('c\x20is\x20a\x20string');
+    break;
+case 'number':
+    console['log']('c\x20is\x20a\x20number');
+    break;
+default:
+    console['log']('c\x20is\x20neither\x20a\x20string\x20nor\x20a\x20number');
+}

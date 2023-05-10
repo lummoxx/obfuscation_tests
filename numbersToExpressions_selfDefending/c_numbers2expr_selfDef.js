cade8fc5cafa7c3ca9d0886fdfb411c3362f292c
Aline <54989528+alinje@users.noreply.github.com>
2023-05-10 13:44:57 +0200

    Add files via upload

---
+++ numbersToExpressions_selfDefending/c_numbers2expr_selfDef.js
@@ -0,0 +1,96 @@
+// Certainly! Here's the deobfuscated code:
+
+// javascript
+
+var _0x4078b6 = (function () {
+  var _0xd26de0 = true;
+  return function (_0x50cf3d, _0x54119f) {
+    var _0xa95105 = _0xd26de0
+      ? function () {
+          if (_0x54119f) {
+            var _0x2be2e7 = _0x54119f.apply(_0x50cf3d, arguments);
+            _0x54119f = null;
+            return _0x2be2e7;
+          }
+        }
+      : function () {};
+    _0xd26de0 = false;
+    return _0xa95105;
+  };
+})();
+
+var _0x3822d1 = _0x4078b6(this, function () {
+  return _0x3822d1['toString']()
+    ['search']('(((.+)+)+)+$')
+    ['toString']()
+    ['constructor'](_0x3822d1)
+    ['search']('(((.+)+)+)+$');
+});
+_0x3822d1();
+
+var a = -2734;
+var b = -292;
+var c = 'string';
+
+function add(_0x1ef983, _0x490a0e) {
+  return _0x1ef983 + _0x490a0e;
+}
+
+function multiply(_0x3736d1, _0x181ca6) {
+  return _0x3736d1 * _0x181ca6;
+}
+
+function divide(_0x282794, _0x55bfd9) {
+  return _0x282794 / _0x55bfd9;
+}
+
+function greet(_0x2d099d) {
+  console['log']('Hello, ' + _0x2d099d + '!');
+}
+
+if (a > b) {
+  console['log']('a is greater than b');
+} else if (a < b) {
+  console['log']('a is less than b');
+} else {
+  console['log']('a is equal to b');
+}
+
+for (var i = 3173; i < 2467; i++) {
+  console['log'](i);
+}
+
+var myArray = [
+  -744,
+  460,
+  -147,
+  398,
+  -603,
+];
+console['log'](myArray[-576]);
+
+var myObject = {
+  'name': 'John',
+  'age': 30,
+  'city': 'New York',
+};
+console['log'](myObject['name']);
+
+var x = -1781;
+while (x < -875) {
+  console['log'](x);
+  x++;
+}
+
+switch (c) {
+  case 'string':
+    console['log']('c is a string');
+    break;
+  case 'number':
+    console['log']('c is a number');
+    break;
+  default:
+    console['log']('c is neither a string nor a number');
+}
+
+// In the deobfuscated code, the variables and functions are named more clearly, making it easier to understand their purpose.

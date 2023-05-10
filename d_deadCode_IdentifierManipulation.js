f668f2f72124d73d259a5841f0a2ce32df757a33
lummoxx <52716403+lummoxx@users.noreply.github.com>
2023-05-10 10:42:05 +0200

    Create d_deadCode_IdentifierManipulation.js

---
+++ d_deadCode_IdentifierManipulation.js
@@ -0,0 +1,68 @@
+var lamisha = julya;
+(function (rozyn, jyheem) {
+  var eghosa = julya;
+  var maroska = rozyn();
+  while (true) {
+    try {
+      var feyisayo = -parseInt(eghosa(346)) / 1 * (-parseInt(eghosa(333)) / 2) + parseInt(eghosa(342)) / 3 + -parseInt(eghosa(352)) / 4 * (parseInt(eghosa(336)) / 5) + parseInt(eghosa(334)) / 6 * (parseInt(eghosa(340)) / 7) + parseInt(eghosa(348)) / 8 + -parseInt(eghosa(341)) / 9 + parseInt(eghosa(350)) / 10 * (-parseInt(eghosa(349)) / 11);
+      if (feyisayo === jyheem) {
+        break;
+      } else {
+        maroska.push(maroska.shift());
+      }
+    } catch (aaliyaa) {
+      maroska.push(maroska.shift());
+    }
+  }
+}(madisynn, 156089));
+var krisi = 5;
+var hosey = 10;
+function madisynn() {
+  var sherile = ["1822392BRAMHe", "2974235HnsVfg", "10wdAEMS", "number", "8BGiTmK", "a is greater than b", "4DxRxwL", "6AJpxGl", "John", "686550GzzYzf", "c is a string", "string", "a is equal to b", "1197637GLseVG", "1264077ibXKcr", "570069lWnjwu", "New York", "c is a number", "log", "126317uCJSuF", "a is less than b"];
+  madisynn = function () {
+    return sherile;
+  };
+  return madisynn();
+}
+var evana = lamisha(338);
+function tavio(velerie) {
+  var silbestre = lamisha;
+  console[silbestre(345)]("Hello, " + velerie + "!");
+}
+if (krisi > hosey) {
+  console[lamisha(345)](lamisha(353));
+} else if (krisi < hosey) {
+  console.log(lamisha(347));
+} else {
+  console[lamisha(345)](lamisha(339));
+}
+for (var ryyan = 0; ryyan < 10; ryyan++) {
+  console[lamisha(345)](ryyan);
+}
+console[lamisha(345)](3);
+var starlett = {name: lamisha(335), _0x1e4e60: 30, city: lamisha(343)};
+console[lamisha(345)](starlett.name);
+var athenea = 0;
+while (athenea < 5) {
+  console[lamisha(345)](athenea);
+  athenea++;
+}
+function julya(chipper, alisya) {
+  var tallie = madisynn();
+  julya = function (davyan, shavar) {
+    davyan = davyan - 333;
+    var avree = tallie[davyan];
+    return avree;
+  };
+  return julya(chipper, alisya);
+}
+switch (evana) {
+  case lamisha(338):
+    console[lamisha(345)](lamisha(337));
+    break;
+  case lamisha(351):
+    console[lamisha(345)](lamisha(344));
+    break;
+  default:
+    console[lamisha(345)]("c is neither a string nor a number");
+}

1946ad317db318050720fa299aedd40f4ce23d1a
lummoxx <52716403+lummoxx@users.noreply.github.com>
2023-05-10 10:41:18 +0200

    Create d_deadCode_StringArray.js

---
+++ d_deadCode_StringArray.js
@@ -0,0 +1,68 @@
+var nicolas = jermani;
+(function (vernamae, marigny) {
+  var tyiesha = jermani;
+  var velta = vernamae();
+  while (true) {
+    try {
+      var alyzia = -parseInt(tyiesha(296)) / 1 * (parseInt(tyiesha(282)) / 2) + -parseInt(tyiesha(286)) / 3 * (parseInt(tyiesha(278)) / 4) + -parseInt(tyiesha(287)) / 5 + parseInt(tyiesha(292)) / 6 * (-parseInt(tyiesha(279)) / 7) + -parseInt(tyiesha(298)) / 8 * (-parseInt(tyiesha(293)) / 9) + parseInt(tyiesha(280)) / 10 + parseInt(tyiesha(290)) / 11;
+      if (alyzia === marigny) {
+        break;
+      } else {
+        velta.push(velta.shift());
+      }
+    } catch (passionate) {
+      velta.push(velta.shift());
+    }
+  }
+}(jesselin, 797918));
+function jermani(evina, nicolis) {
+  var dondra = jesselin();
+  jermani = function (berlie, anaisa) {
+    berlie = berlie - 278;
+    var tobitha = dondra[berlie];
+    return tobitha;
+  };
+  return jermani(evina, nicolis);
+}
+var a = 5;
+var b = 10;
+var c = nicolas(283);
+function jesselin() {
+  var forney = ["1253556ZrXMFV", "number", "c is a number", "173zLixyF", "John", "8xWnSGG", "9596ZJCTUt", "1206597TIzrXj", "14846320bHPDVS", "a is greater than b", "9086KKOZQA", "string", "c is a string", "a is less than b", "1929mkOAtC", "6067310HBpCTj", "New York", "log", "31771641GCbdDu", "Hello, ", "6CCvHAg"];
+  jesselin = function () {
+    return forney;
+  };
+  return jesselin();
+}
+function greet(vanshika) {
+  var damin = nicolas;
+  console[damin(289)](damin(291) + vanshika + "!");
+}
+if (a > b) {
+  console[nicolas(289)](nicolas(281));
+} else if (a < b) {
+  console[nicolas(289)](nicolas(285));
+} else {
+  console.log("a is equal to b");
+}
+for (var i = 0; i < 10; i++) {
+  console[nicolas(289)](i);
+}
+console.log(3);
+var myObject = {name: nicolas(297), age: 30, city: nicolas(288)};
+console.log(myObject.name);
+var x = 0;
+while (x < 5) {
+  console[nicolas(289)](x);
+  x++;
+}
+switch (c) {
+  case nicolas(283):
+    console[nicolas(289)](nicolas(284));
+    break;
+  case nicolas(294):
+    console.log(nicolas(295));
+    break;
+  default:
+    console[nicolas(289)]("c is neither a string nor a number");
+}

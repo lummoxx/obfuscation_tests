9e9ef5477a0a41d5839bb1193fc8306e4c43d412
lummoxx <52716403+lummoxx@users.noreply.github.com>
2023-05-10 10:15:10 +0200

    Create deadCode_StringArray.js

---
+++ deadCode_StringArray.js
@@ -0,0 +1,110 @@
+var _0xa36bb4 = _0xf7d9;
+(function (_0x193654, _0x5ef13c) {
+    var _0xfe91cd = _0xf7d9;
+    var _0x1d18b6 = _0x193654();
+    while (!![]) {
+        try {
+            var _0x266dc0 = -parseInt(_0xfe91cd(0x128)) / 0x1 * (parseInt(_0xfe91cd(0x11a)) / 0x2) + -parseInt(_0xfe91cd(0x11e)) / 0x3 * (parseInt(_0xfe91cd(0x116)) / 0x4) + -parseInt(_0xfe91cd(0x11f)) / 0x5 + parseInt(_0xfe91cd(0x124)) / 0x6 * (-parseInt(_0xfe91cd(0x117)) / 0x7) + -parseInt(_0xfe91cd(0x12a)) / 0x8 * (-parseInt(_0xfe91cd(0x125)) / 0x9) + parseInt(_0xfe91cd(0x118)) / 0xa + parseInt(_0xfe91cd(0x122)) / 0xb;
+            if (_0x266dc0 === _0x5ef13c) {
+                break;
+            } else {
+                _0x1d18b6['push'](_0x1d18b6['shift']());
+            }
+        } catch (_0xe04f45) {
+            _0x1d18b6['push'](_0x1d18b6['shift']());
+        }
+    }
+}(_0x2eb9, 0xc2cde));
+function _0xf7d9(_0x3e821e, _0x501717) {
+    var _0x2eb922 = _0x2eb9();
+    _0xf7d9 = function (_0xf7d911, _0x2f9b31) {
+        _0xf7d911 = _0xf7d911 - 0x116;
+        var _0x111b7b = _0x2eb922[_0xf7d911];
+        return _0x111b7b;
+    };
+    return _0xf7d9(_0x3e821e, _0x501717);
+}
+var a = 0x5;
+var b = 0xa;
+var c = _0xa36bb4(0x11b);
+function add(_0x5c7245, _0x30e414) {
+    return _0x5c7245 + _0x30e414;
+}
+function _0x2eb9() {
+    var _0x3bb4fc = [
+        '1253556ZrXMFV',
+        'number',
+        'c\x20is\x20a\x20number',
+        '173zLixyF',
+        'John',
+        '8xWnSGG',
+        '9596ZJCTUt',
+        '1206597TIzrXj',
+        '14846320bHPDVS',
+        'a\x20is\x20greater\x20than\x20b',
+        '9086KKOZQA',
+        'string',
+        'c\x20is\x20a\x20string',
+        'a\x20is\x20less\x20than\x20b',
+        '1929mkOAtC',
+        '6067310HBpCTj',
+        'New\x20York',
+        'log',
+        '31771641GCbdDu',
+        'Hello,\x20',
+        '6CCvHAg'
+    ];
+    _0x2eb9 = function () {
+        return _0x3bb4fc;
+    };
+    return _0x2eb9();
+}
+function multiply(_0x176a84, _0x40ccbb) {
+    return _0x176a84 * _0x40ccbb;
+}
+function divide(_0xaba2f, _0x5d51d6) {
+    return _0xaba2f / _0x5d51d6;
+}
+function greet(_0x598bcb) {
+    var _0x4b6666 = _0xa36bb4;
+    console[_0x4b6666(0x121)](_0x4b6666(0x123) + _0x598bcb + '!');
+}
+if (a > b) {
+    console[_0xa36bb4(0x121)](_0xa36bb4(0x119));
+} else if (a < b) {
+    console[_0xa36bb4(0x121)](_0xa36bb4(0x11d));
+} else {
+    console['log']('a\x20is\x20equal\x20to\x20b');
+}
+for (var i = 0x0; i < 0xa; i++) {
+    console[_0xa36bb4(0x121)](i);
+}
+var myArray = [
+    0x1,
+    0x2,
+    0x3,
+    0x4,
+    0x5
+];
+console['log'](myArray[0x2]);
+var myObject = {
+    'name': _0xa36bb4(0x129),
+    'age': 0x1e,
+    'city': _0xa36bb4(0x120)
+};
+console['log'](myObject['name']);
+var x = 0x0;
+while (x < 0x5) {
+    console[_0xa36bb4(0x121)](x);
+    x++;
+}
+switch (c) {
+case _0xa36bb4(0x11b):
+    console[_0xa36bb4(0x121)](_0xa36bb4(0x11c));
+    break;
+case _0xa36bb4(0x126):
+    console['log'](_0xa36bb4(0x127));
+    break;
+default:
+    console[_0xa36bb4(0x121)]('c\x20is\x20neither\x20a\x20string\x20nor\x20a\x20number');
+}

85282d69620e5ddd8653adbe79fec56eec1f7128
lummoxx <52716403+lummoxx@users.noreply.github.com>
2023-05-10 11:28:58 +0200

    Create c_deadCode_DebugProtection.js

---
+++ c_deadCode_DebugProtection.js
@@ -0,0 +1,46 @@
+/*
+The provided code is obfuscated and difficult to understand. 
+It consists of a few IIFE (Immediately Invoked Function Expression) calls. 
+Without knowing the specific use case and the context of the code, 
+it is hard to determine the exact purpose of this code. 
+However, I can deobfuscate the code to make it more readable.
+
+Here is the deobfuscated version of the code:
+*/
+
+var _0x329331 = _0xdca2;
+
+(function (_0xb2f89b, _0xedfbc) {
+    var _0x41fe26 = _0xdca2;
+    var _0x378583 = _0xb2f89b();
+    while (!![]) {
+        try {
+            var _0x2389d1 = -parseInt(_0x41fe26(0xb6)) / 0x1 * (parseInt(_0x41fe26(0x9f)) / 0x2) + -parseInt(_0x41fe26(0xac)) / 0x3 * (-parseInt(_0x41fe26(0xaf)) / 0x4) + parseInt(_0x41fe26(0xa0)) / 0x5 + parseInt(_0x41fe26(0xad)) / 0x6 + parseInt(_0x41fe26(0x90)) / 0x7 + parseInt(_0x41fe26(0x91)) / 0x8 + parseInt(_0x41fe26(0xaa)) / 0x9 * (-parseInt(_0x41fe26(0x96)) / 0xa);
+
+            if (_0x2389d1 === _0xedfbc) {
+                break;
+            } else {
+                _0x378583['push'](_0x378583['shift']());
+            }
+        } catch (_0x42381e) {
+            _0x378583['push'](_0x378583['shift']());
+        }
+    }
+}(_0x4851, 0x7c360));
+
+var _0x15c4d2 = (function () {
+    var _0x2796ac = true;
+    return function (_0x240a0d, _0x20e273) {
+        var _0x45cb65 = _0x2796ac ? function () {
+            var _0x57b63f = _0xdca2;
+            if (_0x57b63f(0xbb) !== _0x57b63f(0xbb)) {
+                return _0x89b1f + _0x297b5f;
+            } else {
+                if (_0x20e273) {
+                    if ('XGJyA' !== _0x57b63f(0xb5)) {
+                        _0x4269f5(this, function () {
+                            var _0x2c8bab = _0x57b63f;
+                            var _0x5c19fc = new _0x325326(_0x2c8bab(0x93));
+                            var _0x7e94c = new _0x1beca1(_0x2c8bab(0x98), 'i');
+                            var _0x188ddc = _0x18373c(_0x2c8bab(0xba));
+                            if (!_0x5c19fc[_0x2c8bab(0x97)](_0x188ddc + _0x2c8bab(0x9c)) || !_0x7e94c[_0x2c8

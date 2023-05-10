c49234f65b391a1556849e682a41613aeb62f83b
lummoxx <52716403+lummoxx@users.noreply.github.com>
2023-05-10 12:11:13 +0200

    Create c_controlFlow_IdentifierManipulation.js

---
+++ c_controlFlow_IdentifierManipulation.js
@@ -0,0 +1,66 @@
+// Sure! Here is the deobfuscated code:
+
+var a = 5;
+var b = 10;
+var c = 'string';
+
+function add(x, y) {
+    return x + y;
+}
+
+function multiply(x, y) {
+    return x * y;
+}
+
+function divide(x, y) {
+    return x / y;
+}
+
+function sayHello(name) {
+    console.log('Hello, ' + name + '!');
+}
+
+if (a > b) {
+    console.log('a is greater than b');
+} else if (a < b) {
+    console.log('a is less than b');
+} else {
+    console.log('a is equal to b');
+}
+
+for (var i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+var arr = [1, 2, 3, 4, 5];
+console.log(arr[2]);
+
+var obj = {
+    name: 'John',
+    age: 30,
+    city: 'New York'
+};
+console.log(obj.name);
+
+var counter = 0;
+while (counter < 5) {
+    console.log(counter);
+    counter++;
+}
+
+switch (c) {
+    case 'string':
+        console.log('c is a string');
+        break;
+    case 'number':
+        console.log('c is a number');
+        break;
+    default:
+        console.log('c is neither a string nor a number');
+}
+
+/*
+The obfuscated code was mostly composed of variables and functions with meaningless names, 
+and function calls with cryptic parameter names. The structure and logic of the code 
+remain unchanged after deobfuscation.
+*/

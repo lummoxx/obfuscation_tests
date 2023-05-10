55633b2e2bd8c620898567d68ec432975fbe2633
lummoxx <52716403+lummoxx@users.noreply.github.com>
2023-05-10 11:20:14 +0200

    Create c_deadCode_StringArray.js

---
+++ c_deadCode_StringArray.js
@@ -0,0 +1,93 @@
+//This code appears to have been obfuscated using a technique called "self-defining functions." 
+//The function _0xf7d9 is defined within itself, which makes it difficult to read and understand the code's purpose.
+//Here is the deobfuscated code:
+
+function _0xf7d9(_0x3e821e, _0x501717) {
+    var _0x2eb922 = _0x2eb9();
+    _0xf7d9 = function (_0xf7d911, _0x2f9b31) {
+        _0xf7d911 = _0xf7d911 - 0x116;
+        var _0x111b7b = _0x2eb922[_0xf7d911];
+        return _0x111b7b;
+    };
+    return _0xf7d9(_0x3e821e, _0x501717);
+}
+
+function _0x2eb9() {
+    var _0x3bb4fc = [
+        '1253556ZrXMFV',
+        'number',
+        'c is a number',
+        '173zLixyF',
+        'John',
+        '8xWnSGG',
+        '9596ZJCTUt',
+        '1206597TIzrXj',
+        '14846320bHPDVS',
+        'a is greater than b',
+        '9086KKOZQA',
+        'string',
+        'c is a string',
+        'a is less than b',
+        '1929mkOAtC',
+        '6067310HBpCTj',
+        'New York',
+        'log',
+        '31771641GCbdDu',
+        'Hello, ',
+        '6CCvHAg'
+    ];
+    _0x2eb9 = function () {
+        return _0x3bb4fc;
+    };
+    return _0x2eb9();
+}
+
+var a = 0x5;
+var b = 0xa;
+var c = _0x2eb9()[0x11b];
+
+function add(_0x5c7245, _0x30e414) {
+    return _0x5c7245 + _0x30e414;
+}
+
+function multiply(_0x176a84, _0x40ccbb) {
+    return _0x176a84 * _0x40ccbb;
+}
+
+function divide(_0xaba2f, _0x5d51d6) {
+    return _0xaba2f / _0x5d51d6;
+}
+
+function greet(_0x598bcb) {
+    console.log('Hello, ' + _0x598bcb + '!');
+}
+
+if (a > b) {
+    console.log('a is greater than b');
+} else if (a < b) {
+    console.log('a is less than b');
+} else {
+    console.log('a is equal to b');
+}
+
+for (var i = 0x0; i < 0xa; i++) {
+    console.log(i);
+}
+
+var myArray = [
+    0x1,
+    0x2,
+    0x3,
+    0x4,
+    0x5
+];
+console.log(myArray[0x2]);
+
+var myObject = {
+    'name': 'John',
+    'age': 0x28,
+    'city': 'New York'
+};
+console.log(myObject['city']);
+
+

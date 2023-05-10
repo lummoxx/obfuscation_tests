ce93901ad331fb7c1dc8e75524ebb6356bc8ca23
Aline <54989528+alinje@users.noreply.github.com>
2023-05-10 13:45:41 +0200

    Add files via upload

---
+++ stringArray_ numbersToExpressions/c_stringArr_numbers2expr.js
@@ -0,0 +1,175 @@
+// The deobfuscated code is as follows:
+
+// javascript
+
+var _0x5d9978 = _0x2b8e;
+(function (_0x170f01, _0x40c749) {
+    var _0x2bac52 = _0x2b8e;
+    var _0x56c235 = _0x170f01();
+    while (!![]) {
+        try {
+            var _0x59984f = parseInt(_0x2bac52(0xc3)) / (-0x1b * 0x55 + -0xd78 * 0x1 + -0x480) + -parseInt(_0x2bac52(0xc1)) / (-0x68 * 0x1a + -0x63 * 0x14 + -0x30d * 0x6) + -parseInt(_0x2bac52(0xbd)) / (0x21 * -0x6f + 0x97e + 0x19c * 0x3) * (parseInt(_0x2bac52(0xb5)) / (-0x149 * 0x1b + 0xef9 + 0xca7)) + parseInt(_0x2bac52(0xb7)) / (-0x7ab * 0x2 + -0x1d * 0x79 + -0x2e7 * 0xa) + -parseInt(_0x2bac52(0xaf)) / (0x183a + -0x2303 + 0x1 * 0xacf) * (parseInt(_0x2bac52(0xbf)) / (0x1 * 0x502 + 0x37 * -0x2e + -0xfb * 0x5)) + -parseInt(_0x2bac52(0xbb)) / (0x3 * -0x9af + -0x2084 + -0x4bd * 0xd) * (-parseInt(_0x2bac52(0xc2)) / (-0x1b97 * 0x1 + 0xef9 + 0xca7)) + parseInt(_0x2bac52(0xb0)) / (0x932 + 0x38d * 0x7 + -0x2203) * (parseInt(_0x2bac52(0xc0)) / (-0x138 + 0x3d * 0x53 + -0x5 * 0x3b4));
+            if (_0x59984f === _0x40c749) {
+                break;
+            } else {
+                _0x56c235['push'](_0x56c235['shift']());
+            }
+        } catch (_0x2a1fb9) {
+            _0x56c235['push'](_0x56c235['shift']());
+        }
+    }
+}(_0x1227, -0x1 * 0x5e37a + 0x842fd * -0x1 + 0x343a9 * 0x4));
+var a = 0x135b + 0xb * -0x191 + -0xb * 0x31;
+var b = 0x1d5 + -0x1345 + 0x117a;
+var c = 'string';
+
+function add(_0xdc1745, _0x5023d6) {
+    return _0xdc1745 + _0x5023d6;
+}
+
+function _0x1227() {
+   
+
+// User
+// Could you finish the code?
+// ChatGPT
+
+// Certainly! Here's the complete code:
+
+// javascript
+
+var _0x5d9978 = _0x2b8e;
+(function (_0x170f01, _0x40c749) {
+    var _0x2bac52 = _0x2b8e;
+    var _0x56c235 = _0x170f01();
+    while (!![]) {
+        try {
+            var _0x59984f = parseInt(_0x2bac52(0xc3)) / (-0x1b * 0x55 + -0xd78 * 0x1 + -0x480) + -parseInt(_0x2bac52(0xc1)) / (-0x68 * 0x1a + -0x63 * 0x14 + -0x30d * 0x6) + -parseInt(_0x2bac52(0xbd)) / (0x21 * -0x6f + 0x97e + 0x19c * 0x3) * (parseInt(_0x2bac52(0xb5)) / (-0x149 * 0x1b + 0xef9 + 0xca7)) + parseInt(_0x2bac52(0xb7)) / (-0x7ab * 0x2 + -0x1d * 0x79 + -0x2e7 * 0xa) + -parseInt(_0x2bac52(0xaf)) / (0x183a + -0x2303 + 0x1 * 0xacf) * (parseInt(_0x2bac52(0xbf)) / (0x1 * 0x502 + 0x37 * -0x2e + -0xfb * 0x5)) + -parseInt(_0x2bac52(0xbb)) / (0x3 * -0x9af + -0x2084 + -0x4bd * 0xd) * (-parseInt(_0x2bac52(0xc2)) / (-0x1b97 * 0x1 + 0xef9 + 0xca7)) + parseInt(_0x2bac52(0xb0)) / (0x932 + 0x38d * 0x7 + -0x2203) * (parseInt(_0x2bac52(0xc0)) / (-0x138 + 0x3d * 0x53 + -0x5 * 0x3b4));
+            if (_0x59984f === _0x40c749) {
+                break;
+            } else {
+                _0x56c235['push'](_0x56c235['shift']());
+            }
+        } catch (_0x2a1fb9) {
+            _0x56c235['push'](_0x56c235['shift']());
+        }
+    }
+}(_0x1227, -0x1 * 0x5e37a + 0x842fd * -0x1 + 0x343a9 * 0x4));
+
+var a = 0x135b + 0xb * -0x191 + -0xb * 0x31;
+var b = 0x1d5 + -0x1345 + 0x117a;
+var c = 'string';
+
+function add(_0xdc1745, _0x5023d6) {
+    return _0xdc1745 + _0x5023d6;
+}
+
+function _0x1227() {
+    var
+
+// User
+// Could you give me the end of the code?
+// ChatGPT
+
+// Certainly! Here's the rest of the code:
+
+// javascript
+
+    var _0x1990bd = [
+        'a is greater than b',
+        '5693980QiRgdI',
+        'c is a string',
+        'c is a number',
+        'a is equal to b',
+        '536bXLplH',
+        'c is neither a string nor a number',
+        '2157153zBzEFl',
+        'number',
+        '9066631cVYkyO',
+        '33oYWHUN',
+        '2615880yXRfNc',
+        '144009INyOIR',
+        '1212979IVlviT',
+        '6zuRZXW',
+        '1995250ehyYkN',
+        'string',
+        'John',
+        'Hello, ',
+        'log',
+        '4yzyArO'
+    ];
+    _0x1227 = function () {
+        return _0x1990bd;
+    };
+    return _0x1227();
+}
+
+function multiply(_0x118053, _0x58047c) {
+    return _0x118053 * _0x58047c;
+}
+
+function divide(_0x1b596c, _0x2e6e0a) {
+    return _0x1b596c / _0x2e6e0a;
+}
+
+function greet(_0x322045) {
+    var _0xbc7928 = _0x2b8e;
+    console.log(_0xbc7928(0xb3) + _0x322045 + '!');
+}
+
+if (a > b) {
+    console.log('a is greater than b');
+} else if (a < b) {
+    console.log('a is less than b');
+} else {
+    console.log('a is equal to b');
+}
+
+for (var i = -0x1b65 * 0x1 + 0x7 * -0x571 + -0xab2 * 0x1; i < 0x1e * 0x1b + 0x212 * 0x4 + -0xb68; i++) {
+    console.log(i);
+}
+
+var myArray = [
+    -0x373 * 0x2 + 0xd01 * -0x2 + 0xe9 * 0x15,
+    0x4 * -0x1f5 + -0x247d * 0x1 + 0x2c53,
+    -0x1 * 0xefb + 0x1b40 + -0x2a38,
+    0x416 + 0x2175 + -0xd * 0x2e3,
+    -0x205 + 0x12ce + -0x2 * 0x862
+];
+console.log(myArray[-0x251e + 0x377 * 0x9 + 0x9 * 0xa9]);
+
+var myObject = {
+    'name': 'John',
+    'age': 0x1e,
+    'city': 'New York'
+};
+console.log(myObject['name']);
+
+var x = -0x100b + -0xb2a + 0x1b35;
+while (x < 0xbe * 0x2 + -0x4df * 0x8 + -0x286f) {
+    console.log(x);
+    x++;
+}
+
+switch (c) {
+    case 'string':
+        console.log('c is a string');
+        break;
+    case 'number':
+        console.log
+
+// User
+// What does the code do?
+// ChatGPT
+
+/* The code performs the following actions:
+
+    Defines a set of utility functions: multiply, divide, greet.
+    Compares the values of variables a and b and logs a message indicating whether a is greater than, less than, or equal to b.
+    Executes a loop that logs the values of variable i from -0x1b65 to 0x1e * 0x1b + 0x212 * 0x4 + -0xb68.
+    Defines an array myArray and logs the value at index -0x251e + 0x377 * 0x9 + 0x9 * 0xa9.
+    Defines an object myObject with properties name, age, and city, and logs the value of the name property.
+    Executes a loop that logs the values of variable x from -0x100b to 0xbe * 0x2 + -0x4df * 0x8 + -0x286f.
+    Uses a switch statement to check the value of variable c and logs a message indicating whether c is a string, a number, or neither.
+
+Overall, the code demonstrates basic JavaScript concepts such as variable declaration, conditional statements, loops, arrays, objects, and function definitions. */

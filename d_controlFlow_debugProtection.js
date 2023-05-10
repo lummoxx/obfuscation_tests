8450bdea0d881ecfcf8a6e4eae7cba0011e3006f
lummoxx <52716403+lummoxx@users.noreply.github.com>
2023-05-10 10:49:48 +0200

    Create d_controlFlow_debugProtection.js

---
+++ d_controlFlow_debugProtection.js
@@ -0,0 +1,148 @@
+var palynn = function () {
+  var dequavion = true;
+  return function (nataniel, aneliese) {
+    var jakiria = dequavion ? function () {
+      if (aneliese) {
+        var maire = aneliese.apply(nataniel, arguments);
+        aneliese = null;
+        return maire;
+      }
+    } : function () {};
+    dequavion = false;
+    return jakiria;
+  };
+}();
+(function () {
+  var sydnea = {kptkE: function (asjha, meixi) {
+    return asjha(meixi);
+  }, VsiUQ: function (chalissa, vinie) {
+    return chalissa + vinie;
+  }, jHmYE: "return (function() ", ZZksn: '{}.constructor("return this")( )', NugWz: function (mathew) {
+    return mathew();
+  }};
+  var serrina = function () {
+    var dsean;
+    try {
+      dsean = sydnea.kptkE(Function, sydnea.VsiUQ(sydnea.jHmYE + sydnea.ZZksn, ");"))();
+    } catch (pernella) {
+      dsean = window;
+    }
+    return dsean;
+  };
+  var azaiya = sydnea.NugWz(serrina);
+  azaiya.setInterval(lyssette, 2e3);
+}());
+(function () {
+  var keylanie = {iLQpL: "function *\\( *\\)", kwiAh: "\\+\\+ *(?:[a-zA-Z_$][0-9a-zA-Z_$]*)", kTNSL: function (omunique, zorria) {
+    return omunique(zorria);
+  }, LlFyE: function (willella, larkyn) {
+    return willella + larkyn;
+  }, bAGIU: "chain", lMNTb: function (jollette, laylee, ambriah) {
+    return jollette(laylee, ambriah);
+  }};
+  keylanie.lMNTb(palynn, this, function () {
+    var vaughn = new RegExp(keylanie.iLQpL);
+    var roverta = new RegExp(keylanie.kwiAh, "i");
+    var shaula = keylanie.kTNSL(lyssette, "init");
+    if (!vaughn.test(keylanie.LlFyE(shaula, keylanie.bAGIU)) || !roverta.test(shaula + "input")) {
+      keylanie.kTNSL(shaula, "0");
+    } else {
+      lyssette();
+    }
+  })();
+}());
+var a = 5;
+var b = 10;
+var c = "string";
+function add(linsy, waco) {
+  var charnese = {HPGya: function (harron, alayasia) {
+    return harron + alayasia;
+  }};
+  return charnese.HPGya(linsy, waco);
+}
+function multiply(keli, elvan) {
+  var ladonne = {cazgP: function (mays, yitzhak) {
+    return mays * yitzhak;
+  }};
+  return ladonne.cazgP(keli, elvan);
+}
+function divide(coast, coraliz) {
+  var alisi = {KDtZX: function (claudette, gursanjh) {
+    return claudette / gursanjh;
+  }};
+  return alisi.KDtZX(coast, coraliz);
+}
+function greet(venis) {
+  var judia = {QtGJq: function (allana, stepfan) {
+    return allana + stepfan;
+  }, SpWqb: "Hello, "};
+  console.log(judia.QtGJq(judia.QtGJq(judia.SpWqb, venis), "!"));
+}
+if (a > b) {
+  console.log("a is greater than b");
+} else if (a < b) {
+  console.log("a is less than b");
+} else {
+  console.log("a is equal to b");
+}
+for (var i = 0; i < 10; i++) {
+  console.log(i);
+}
+console.log(3);
+var myObject = {name: "John", age: 30, city: "New York"};
+console.log(myObject.name);
+var x = 0;
+while (x < 5) {
+  console.log(x);
+  x++;
+}
+switch (c) {
+  case "string":
+    console.log("c is a string");
+    break;
+  case "number":
+    console.log("c is a number");
+    break;
+  default:
+    console.log("c is neither a string nor a number");
+}
+function lyssette(lavon) {
+  var dexton = {DDBmJ: function (deshanda, alprentice) {
+    return deshanda === alprentice;
+  }, YDvrc: "string", Pdycg: "while (true) {}", NTZOW: "counter", gNkJL: function (annakat, kaley) {
+    return annakat + kaley;
+  }, Bhkxf: function (mykenzee, jacks) {
+    return mykenzee / jacks;
+  }, eVBfq: function (jabar, tavares) {
+    return jabar % tavares;
+  }, bzVzM: function (markcus, lisaann) {
+    return markcus + lisaann;
+  }, heWaR: "debu", EuHyO: "action", zKdiI: function (oniel, kieren) {
+    return oniel + kieren;
+  }, NJlTY: "gger", jHMLV: "stateObject", OOuCl: function (tymere, anan) {
+    return tymere(anan);
+  }};
+  function juliyan(jelina) {
+    if (dexton.DDBmJ(typeof jelina, dexton.YDvrc)) {
+      return function (charquita) {}.constructor(dexton.Pdycg).apply(dexton.NTZOW);
+    } else {
+      if (dexton.gNkJL("", dexton.Bhkxf(jelina, jelina)).length !== 1 || dexton.eVBfq(jelina, 20) === 0) {
+        (function () {
+          return true;
+        }.constructor(dexton.bzVzM(dexton.heWaR, "gger")).call(dexton.EuHyO));
+      } else {
+        (function () {
+          return false;
+        }.constructor(dexton.zKdiI("debu", dexton.NJlTY)).apply(dexton.jHMLV));
+      }
+    }
+    dexton.OOuCl(juliyan, ++jelina);
+  }
+  try {
+    if (lavon) {
+      return juliyan;
+    } else {
+      dexton.OOuCl(juliyan, 0);
+    }
+  } catch (lakoda) {}
+}

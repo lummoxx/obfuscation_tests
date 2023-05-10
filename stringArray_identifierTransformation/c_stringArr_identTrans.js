77ce7899178a673c6b7d8dd6201bc1546621891f
Aline <54989528+alinje@users.noreply.github.com>
2023-05-10 13:45:27 +0200

    Add files via upload

---
+++ stringArray_identifierTransformation/c_stringArr_identTrans.js
@@ -0,0 +1,241 @@
+// Could you deobfuscate the following obfuscated code?
+
+var _0x82273a = _0x3493;
+(function (_0x231929, _0xae3c24) {
+    var _0x19f948 = _0x3493;
+    var _0x29d83f = _0x231929();
+    while (!![]) {
+        try {
+            var _0x2395f7 = parseInt(_0x19f948(0xc4)) / 0x1 + -parseInt(_0x19f948(0xcb)) / 0x2 + parseInt(_0x19f948(0xd2)) / 0x3 + -parseInt(_0x19f948(0xc3)) / 0x4 + parseInt(_0x19f948(0xd0)) / 0x5 * (parseInt(_0x19f948(0xca)) / 0x6) + parseInt(_0x19f948(0xc9)) / 0x7 + parseInt(_0x19f948(0xc2)) / 0x8 * (-parseInt(_0x19f948(0xc8)) / 0x9);
+            if (_0x2395f7 === _0xae3c24) {
+                break;
+            } else {
+                _0x29d83f['push'](_0x29d83f['shift']());
+            }
+        } catch (_0x3b20c8) {
+            _0x29d83f['push'](_0x29d83f['shift']());
+        }
+    }
+}(_0x2fbc, 0xad598));
+var _0x65870d = 0x5;
+function _0x2fbc() {
+    var _0x523cf9 = [
+        '9891945EkmaoI',
+        '6546VMFxFp',
+        '2574890fSKKXc',
+        'Hello,\x20',
+        'a\x20is\x20less\x20than\x20b',
+        'string',
+        'name',
+        '4565EUTdjF',
+        'log',
+        '352872heScgC',
+        'a\x20is\x20greater\x20than\x20b',
+        'John',
+        '16UIrDMx',
+        '2902364tvSiJF',
+        '405110lYzphu',
+        'a\x20is\x20equal\x20to\x20b',
+        'c\x20is\x20a\x20number',
+        'number',
+        '939942ArsvBQ'
+    ];
+    _0x2fbc = function () {
+        return _0x523cf9;
+    };
+    return _0x2fbc();
+}
+var _0x11ce42 = 0xa;
+var _0x1c2d78 = _0x82273a(0xce);
+function _0x547072(_0x25b14d, _0x2f5c1a) {
+    return _0x25b14d + _0x2f5c1a;
+}
+function _0x1c702f(_0x2e1c32, _0x3901dd) {
+    return _0x2e1c32 * _0x3901dd;
+}
+function _0x1e66a3(_0x1dda10, _0x3d6801) {
+    return _0x1dda10 / _0x3d6801;
+}
+function _0x5be4f1(_0x4aa20b) {
+    var _0x481912 = _0x82273a;
+    console[_0x481912(0xd1)](_0x481912(0xcc) + _0x4aa20b + '!');
+}
+if (_0x65870d > _0x11ce42) {
+    console[_0x82273a(0xd1)](_0x82273a(0xd3));
+} else if (_0x65870d < _0x11ce42) {
+    console[_0x82273a(0xd1)](_0x82273a(0xcd));
+} else {
+    console[_0x82273a(0xd1)](_0x82273a(0xc5));
+}
+function _0x3493(_0x2a36bc, _0x2dcc4f) {
+    var _0x2fbcab = _0x2fbc();
+    _0x3493 = function (_0x3493d8, _0xeb659) {
+        _0x3493d8 = _0x3493d8 - 0xc1;
+        var _0x47c1c5 = _0x2fbcab[_0x3493d8];
+        return _0x47c1c5;
+    };
+    return _0x3493(_0x2a36bc, _0x2dcc4f);
+}
+for (var _0x18452b = 0x0; _0x18452b < 0xa; _0x18452b++) {
+    console[_0x82273a(0xd1)](_0x18452b);
+}
+var _0x2fb6a0 = [
+    0x1,
+    0x2,
+    0x3,
+    0x4,
+    0x5
+];
+console[_0x82273a(0xd1)](_0x2fb6a0[0x2]);
+var _0x12d388 = {
+    'name': _0x82273a(0xc1),
+    '_0x47085b': 0x1e,
+    'city': 'New\x20York'
+};
+console[_0x82273a(0xd1)](_0x12d388[_0x82273a(0xcf)]);
+var _0x31d3c7 = 0x0;
+while (_0x31d3c7 < 0x5) {
+    console[_0x82273a(0xd1)](_0x31d3c7);
+    _0x31d3c7++;
+}
+switch (_0x1c2d78) {
+case 'string':
+    console[_0x82273a(0xd1)]('c\x20is\x20a\x20string');
+    break;
+case _0x82273a(0xc7):
+    console[_0x82273a(0xd1)](_0x82273a(0xc6));
+    break;
+default:
+    console[_0x82273a(0xd1)]('c\x20is\x20neither\x20a\x20string\x20nor\x20a\x20number');
+}
+ChatGPT
+
+The deobfuscated code is as follows:
+
+javascript
+
+var _0x82273a = _0x3493;
+(function (_0x231929, _0xae3c24) {
+    var _0x19f948 = _0x3493;
+    var _0x29d83f = _0x231929();
+    while (!![]) {
+        try {
+            var _0x2395f7 = parseInt(_0x19f948(0xc4)) / 0x1 + -parseInt(_0x19f948(0xcb)) / 0x2 + parseInt(_0x19f948(0xd2)) / 0x3 + -parseInt(_0x19f948(0xc3)) / 0x4 + parseInt(_0x19f948(0xd0)) / 0x5 * (parseInt(_0x19f948(0xca)) / 0x6) + parseInt(_0x19f948(0xc9)) / 0x7 + parseInt(_0x19f948(0xc2)) / 0x8 * (-parseInt(_0x19f948(0xc8)) / 0x9);
+            if (_0x2395f7 === _0xae3c24) {
+                break;
+            } else {
+                _0x29d83f['push'](_0x29d83f['shift']());
+            }
+        } catch (_0x3b20c8) {
+            _0x29d83f['push'](_0x29d83f['shift']());
+        }
+    }
+}(_0x2fbc, 0xad598));
+var _0x65870d = 0x5;
+
+function _0x2fbc() {
+    var _0x523cf9 = [
+        '9891945EkmaoI',
+        '6546VMFxFp',
+        '2574890fSKKXc',
+        'Hello,\x20',
+        'a\x20is\x20less\x20than\x20b',
+        'string',
+        'name',
+        '4565EUTdjF',
+        'log',
+        '352872heScgC',
+        'a\x20is\x20greater\x20than\x20b',
+        'John',
+        '16UIrDMx',
+        '2902364tvSiJF',
+        '405110lYzphu',
+        'a\x20is\x20equal\x20to\x20b',
+        'c\x20is\x20a\x20number',
+        'number',
+        '939942ArsvBQ'
+    ];
+    _0x2fbc = function () {
+        return _0x523cf9;
+    };
+    return _0x2fbc();
+}
+
+var _0x11ce42 = 0xa;
+var _0x1c2d78 = _0x82273a(0xce);
+
+function _0x547072(_0x25b14d, _0x2f5c1a) {
+    return _0x25b14d + _0x2f5c1a;
+}
+
+function _0x1c702f(_0x2e1c32, _0x3901dd) {
+    return _0x2e1c32 * _0x3901dd;
+}
+
+function _0x1e66a3(_0x1dda10, _0x3d6801) {
+    return _0x1dda10 / _0x3d6801;
+}
+
+function _0x5be4
+
+// User
+// Could you finish the code?
+// ChatGPT
+
+// Certainly! Here's the complete code:
+
+// javascript
+
+function _0x5be4f1(_0x4aa20b) {
+    var _0x481912 = _0x82273a;
+    console[_0x481912(0xd1)](_0x481912(0xcc) + _0x4aa20b + '!');
+}
+
+if (_0x65870d > _0x11ce42) {
+    console[_0x82273a(0xd1)](_0x82273a(0xd3));
+} else if (_0x65870d < _0x11ce42) {
+    console[_0x82273a(0xd1)](_0x82273a(0xcd));
+} else {
+    console[_0x82273a(0xd1)](_0x82273a(0xc5));
+}
+
+for (var _0x18452b = 0x0; _0x18452b < 0xa; _0x18452b++) {
+    console[_0x82273a(0xd1)](_0x18452b);
+}
+
+var _0x2fb6a0 = [
+    0x1,
+    0x2,
+    0x3,
+    0x4,
+    0x5
+];
+
+console[_0x82273a(0xd1)](_0x2fb6a0[0x2]);
+
+var _0x12d388 = {
+    'name': 'John',
+    '_0x47085b': 0x1e,
+    'city': 'New\x20York'
+};
+
+console[_0x82273a(0xd1)](_0x12d388[_0x82273a(0xcf)]);
+
+var _0x31d3c7 = 0x0;
+while (_0x31d3c7 < 0x5) {
+    console[_0x82273a(0xd1)](_0x31d3c7);
+    _0x31d3c7++;
+}
+
+switch (_0x1c2d78) {
+    case 'string':
+        console[_0x82273a(0xd1)]('c\x20is\x20a\x20string');
+        break;
+    case 'number':
+        console[_0x82273a(0xd1)]('c\x20is\x20a\x20number');
+        break;
+    default:
+        console[_0x82273a(0xd1)]('c\x20is\x20neither\x20a\x20string\x20nor\x20a\x20number');
+}
+
+// In this code, the _0x5be4f1 function logs a message with the input parameter. The subsequent if-else statement compares the values of _0x65870d and _0x11ce42 and logs a corresponding message. The for loop logs numbers from 0 to 9. The _0x2fb6a0 array is defined with values 1 to 5, and the code logs the value at index 2. The _0x12d388 object has properties name, _0x47085b, and city, and the code logs the value of the name property. The while loop logs numbers from 0 to 4. Finally, the switch statement checks the value of _0x1c2d78 and logs messages based on the case.

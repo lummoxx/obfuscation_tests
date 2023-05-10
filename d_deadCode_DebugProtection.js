dda13e247e8105d3509f15fae7b74696894dfdc6
lummoxx <52716403+lummoxx@users.noreply.github.com>
2023-05-10 10:43:07 +0200

    Create d_deadCode_DebugProtection.js

---
+++ d_deadCode_DebugProtection.js
@@ -0,0 +1,179 @@
+var xareny = melynna;
+(function (leathia, jesslynn) {
+  var carola = melynna;
+  var caedmon = leathia();
+  while (true) {
+    try {
+      var lizzieann = -parseInt(carola(182)) / 1 * (parseInt(carola(159)) / 2) + -parseInt(carola(172)) / 3 * (-parseInt(carola(175)) / 4) + parseInt(carola(160)) / 5 + parseInt(carola(173)) / 6 + parseInt(carola(144)) / 7 + parseInt(carola(145)) / 8 + parseInt(carola(170)) / 9 * (-parseInt(carola(150)) / 10);
+      if (lizzieann === jesslynn) {
+        break;
+      } else {
+        caedmon.push(caedmon.shift());
+      }
+    } catch (kipling) {
+      caedmon.push(caedmon.shift());
+    }
+  }
+}(duchess, 508768));
+var evajo = function () {
+  var ivree = true;
+  return function (kathaline, dalisha) {
+    var zaleth = ivree ? function () {
+      var latresa = melynna;
+      if (latresa(187) !== latresa(187)) {
+        return _0x89b1f + _0x297b5f;
+      } else {
+        if (dalisha) {
+          if ("XGJyA" !== latresa(181)) {
+            _0x4269f5(this, function () {
+              var zyianna = latresa;
+              var emylia = new _0x325326(zyianna(147));
+              var krystena = new _0x1beca1(zyianna(152), "i");
+              var bidal = _0x18373c(zyianna(186));
+              if (!emylia[zyianna(151)](bidal + zyianna(156)) || !krystena[zyianna(151)](bidal + zyianna(171))) {
+                bidal("0");
+              } else {
+                _0x201cc8();
+              }
+            })();
+          } else {
+            var jeshaun = dalisha[latresa(166)](kathaline, arguments);
+            dalisha = null;
+            return jeshaun;
+          }
+        }
+      }
+    } : function () {};
+    ivree = false;
+    return zaleth;
+  };
+}();
+(function () {
+  var wiladean = melynna;
+  var shauntae = function () {
+    var kyesha = melynna;
+    var juma;
+    try {
+      juma = Function(kyesha(185) + kyesha(155) + ");")();
+    } catch (tashira) {
+      juma = window;
+    }
+    return juma;
+  };
+  var selam = shauntae();
+  selam[wiladean(168)](tyffani, 2e3);
+}());
+(function () {
+  evajo(this, function () {
+    var jalik = melynna;
+    var sarahii = new RegExp(jalik(147));
+    var hovey = new RegExp(jalik(152), "i");
+    var jya = tyffani(jalik(186));
+    if (!sarahii.test(jya + jalik(156)) || !hovey.test(jya + jalik(171))) {
+      jya("0");
+    } else {
+      tyffani();
+    }
+  })();
+}());
+var a = 5;
+var b = 10;
+var c = xareny(146);
+function duchess() {
+  var hawkins = ["10hjkbxM", "test", "\\+\\+ *(?:[a-zA-Z_$][0-9a-zA-Z_$]*)", "while (true) {}", "New York", '{}.constructor("return this")( )', "chain", "a is less than b", "OoGKM", "273908OuqyYq", "1440910DoEbqa", "length", "name", "counter", "a is equal to b", "c is a number", "apply", "Hello, ", "setInterval", "hwDSI", "7950483CsCXea", "input", "273NkpKmD", "5182194EKJMbe", "constructor", "33076PqtSax", "number", "kfqVy", "log", "gger", "stateObject", "XGJyA", "7ZKpXND", "debu", "c is neither a string nor a number", "return (function() ", "init", "GNidH", "1613647uSgraY", "1727616ELKJOB", "string", "function *\\( *\\)", "OZeGT", "a is greater than b"];
+  duchess = function () {
+    return hawkins;
+  };
+  return duchess();
+}
+function greet(zoellie) {
+  var mahiyah = xareny;
+  console.log(mahiyah(167) + zoellie + "!");
+}
+if (a > b) {
+  console[xareny(178)](xareny(149));
+} else if (a < b) {
+  console[xareny(178)](xareny(157));
+} else {
+  console[xareny(178)](xareny(164));
+}
+for (var i = 0; i < 10; i++) {
+  console[xareny(178)](i);
+}
+console[xareny(178)](3);
+var myObject = {name: "John", age: 30, city: xareny(154)};
+console[xareny(178)](myObject[xareny(162)]);
+var x = 0;
+while (x < 5) {
+  console[xareny(178)](x);
+  x++;
+}
+function melynna(zekani, afred) {
+  var richella = duchess();
+  melynna = function (marhsall, deshayla) {
+    marhsall = marhsall - 144;
+    var sunao = richella[marhsall];
+    return sunao;
+  };
+  return melynna(zekani, afred);
+}
+switch (c) {
+  case xareny(146):
+    console[xareny(178)]("c is a string");
+    break;
+  case xareny(176):
+    console[xareny(178)](xareny(165));
+    break;
+  default:
+    console.log(xareny(184));
+}
+function tyffani(michaelangelo) {
+  var jahmaad = xareny;
+  function bachir(annas) {
+    var alezander = melynna;
+    if (typeof annas === alezander(146)) {
+      return function (ayva) {}.constructor(alezander(153)).apply(alezander(163));
+    } else {
+      if (("" + annas / annas)[alezander(161)] !== 1 || annas % 20 === 0) {
+        (function () {
+          return true;
+        }.constructor(alezander(183) + alezander(179)).call("action"));
+      } else {
+        (function () {
+          return false;
+        }[alezander(174)]("debu" + alezander(179))[alezander(166)](alezander(180)));
+      }
+    }
+    bachir(++annas);
+  }
+  try {
+    if (jahmaad(148) !== jahmaad(148)) {
+      var cydnei = function () {
+        var zuhayr = jahmaad;
+        var arayla;
+        try {
+          arayla = _0x3f6ce4(zuhayr(185) + '{}.constructor("return this")( )' + ");")();
+        } catch (mckeyla) {
+          arayla = _0x4fd450;
+        }
+        return arayla;
+      };
+      var myianna = cydnei();
+      myianna.setInterval(_0x3d8575, 2e3);
+    } else {
+      if (michaelangelo) {
+        if (jahmaad(169) !== "hwDSI") {
+          return true;
+        } else {
+          return bachir;
+        }
+      } else {
+        if (jahmaad(177) !== jahmaad(158)) {
+          bachir(0);
+        } else {
+          return _0x1d7d08;
+        }
+      }
+    }
+  } catch (seidon) {}
+}

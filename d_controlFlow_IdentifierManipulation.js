0a67c3ea61fcf8c2829559b0472243a14178e140
lummoxx <52716403+lummoxx@users.noreply.github.com>
2023-05-10 10:48:20 +0200

    Create d_controlFlow_IdentifierManipulation.js

---
+++ d_controlFlow_IdentifierManipulation.js
@@ -0,0 +1,57 @@
+var anieta = 5;
+var taelyn = 10;
+var shawana = "string";
+function durland(mattalynn, geffory) {
+  var tydarius = {WqTnb: function (pars, zabrien) {
+    return pars + zabrien;
+  }};
+  return tydarius.WqTnb(mattalynn, geffory);
+}
+function jibri(ryka, ilene) {
+  var pharon = {zKCRd: function (ord, anterrius) {
+    return ord * anterrius;
+  }};
+  return pharon.zKCRd(ryka, ilene);
+}
+function nisan(anayshia, melad) {
+  var kevana = {OTKHT: function (nataliyah, mena) {
+    return nataliyah / mena;
+  }};
+  return kevana.OTKHT(anayshia, melad);
+}
+function pearletta(somi) {
+  var brieona = {IhOdw: function (angenique, narcie) {
+    return angenique + narcie;
+  }, Ilntp: function (paisyn, jaqualla) {
+    return paisyn + jaqualla;
+  }};
+  console.log(brieona.IhOdw(brieona.Ilntp("Hello, ", somi), "!"));
+}
+if (anieta > taelyn) {
+  console.log("a is greater than b");
+} else if (anieta < taelyn) {
+  console.log("a is less than b");
+} else {
+  console.log("a is equal to b");
+}
+for (var iosefa = 0; iosefa < 10; iosefa++) {
+  console.log(iosefa);
+}
+console.log(3);
+var tyronesha = {name: "John", _0x44fe91: 30, city: "New York"};
+console.log(tyronesha.name);
+var demaine = 0;
+while (demaine < 5) {
+  console.log(demaine);
+  demaine++;
+}
+switch (shawana) {
+  case "string":
+    console.log("c is a string");
+    break;
+  case "number":
+    console.log("c is a number");
+    break;
+  default:
+    console.log("c is neither a string nor a number");
+}

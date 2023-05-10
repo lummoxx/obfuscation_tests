9f13976948af7927fe6846a9173d77ba28bd96ab
lummoxx <52716403+lummoxx@users.noreply.github.com>
2023-05-10 10:47:11 +0200

    Create d_controlFlow_stringArray.js

---
+++ d_controlFlow_stringArray.js
@@ -0,0 +1,93 @@
+var faiha = jasavion;
+(function (moree, moni) {
+  var jima = jasavion;
+  var zakeia = moree();
+  while (true) {
+    try {
+      var harvis = -parseInt(jima(139)) / 1 * (parseInt(jima(137)) / 2) + parseInt(jima(138)) / 3 + parseInt(jima(154)) / 4 * (parseInt(jima(141)) / 5) + parseInt(jima(155)) / 6 * (parseInt(jima(147)) / 7) + -parseInt(jima(145)) / 8 + parseInt(jima(140)) / 9 + -parseInt(jima(149)) / 10;
+      if (harvis === moni) {
+        break;
+      } else {
+        zakeia.push(zakeia.shift());
+      }
+    } catch (minisha) {
+      zakeia.push(zakeia.shift());
+    }
+  }
+}(laterica, 869269));
+var a = 5;
+var b = 10;
+var c = faiha(160);
+function add(geraline, oressa) {
+  var izhan = faiha;
+  var sarayah = {fOmny: function (yaritzy, sherwin) {
+    return yaritzy + sherwin;
+  }};
+  return sarayah[izhan(135)](geraline, oressa);
+}
+function laterica() {
+  var nehamiah = ["2450RSmtkR", "a is less than b", "6023370zzrDan", "viMCa", "name", "log", "Hello, ", "12bWVhAf", "282UFeiJD", "number", "bBeSk", "NjZZE", "c is a number", "string", "fOmny", "a is greater than b", "283226wIpIgs", "4179801tqFByM", "1RxHggA", "9300420EayIkh", "1446465UAjwtS", "c is a string", "New York", "Uispp", "13582056bKPNuJ", "c is neither a string nor a number"];
+  laterica = function () {
+    return nehamiah;
+  };
+  return laterica();
+}
+function multiply(charnae, raiyan) {
+  var zebula = faiha;
+  var usmaan = {Uispp: function (paytience, delorse) {
+    return paytience * delorse;
+  }};
+  return usmaan[zebula(144)](charnae, raiyan);
+}
+function divide(tamaka, youseph) {
+  var zykerria = {LHosU: function (marishia, kiomara) {
+    return marishia / kiomara;
+  }};
+  return zykerria.LHosU(tamaka, youseph);
+}
+function greet(tromaine) {
+  var damita = faiha;
+  var mckynna = {viMCa: function (angelyce, medhansh) {
+    return angelyce + medhansh;
+  }, bBeSk: function (slyvia, kately) {
+    return slyvia + kately;
+  }, NjZZE: damita(153)};
+  console[damita(152)](mckynna[damita(150)](mckynna[damita(157)](mckynna[damita(158)], tromaine), "!"));
+}
+if (a > b) {
+  console[faiha(152)](faiha(136));
+} else if (a < b) {
+  console[faiha(152)](faiha(148));
+} else {
+  console[faiha(152)]("a is equal to b");
+}
+for (var i = 0; i < 10; i++) {
+  console[faiha(152)](i);
+}
+function jasavion(jovonne, annily) {
+  var apolinar = laterica();
+  jasavion = function (honor, bronc) {
+    honor = honor - 135;
+    var lydya = apolinar[honor];
+    return lydya;
+  };
+  return jasavion(jovonne, annily);
+}
+console[faiha(152)](3);
+var myObject = {name: "John", age: 30, city: faiha(143)};
+console[faiha(152)](myObject[faiha(151)]);
+var x = 0;
+while (x < 5) {
+  console.log(x);
+  x++;
+}
+switch (c) {
+  case faiha(160):
+    console[faiha(152)](faiha(142));
+    break;
+  case faiha(156):
+    console[faiha(152)](faiha(159));
+    break;
+  default:
+    console[faiha(152)](faiha(146));
+}

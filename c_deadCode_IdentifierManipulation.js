904dc70554cbe8cceb1d079d37aa20a1b3c72a60
lummoxx <52716403+lummoxx@users.noreply.github.com>
2023-05-10 11:26:46 +0200

    Create c_deadCode_IdentifierManipulation.js

---
+++ c_deadCode_IdentifierManipulation.js
@@ -0,0 +1,45 @@
+//The provided code is a mix of obfuscated and unobfuscated code. 
+// The obfuscation technique used is variable name mangling, 
+// where variable names are replaced with randomly generated names. 
+//Here's the deobfuscated code:
+
+function main() {
+  var a = 5;
+  var b = 10;
+
+  if (a > b) {
+    console.log("a is greater than b");
+  } else if (a < b) {
+    console.log("a is less than b");
+  } else {
+    console.log("a is equal to b");
+  }
+
+  for (var i = 0; i < 10; i++) {
+    console.log(i);
+  }
+
+  var arr = [1, 2, 3, 4, 5];
+  console.log(arr[2]);
+
+  var obj = {
+    name: "John",
+    age: 30,
+    city: "New York"
+  };
+
+  console.log("Hello, " + obj.name + "!");
+}
+
+main();
+/*
+
+The code defines a function main() that performs the following tasks:
+
+1. Sets the value of a to 5 and the value of b to 10.
+2. Checks whether a is greater than, less than, or equal to b and logs the appropriate message to the console.
+3. Logs the numbers 0 through 9 to the console.
+4. Defines an array arr with the values 1 through 5 and logs the value at index 2 to the console.
+5. Defines an object obj with properties name, age, and city, and logs a message to the console that includes the value of obj.name.
+6. Calls the main() function.
+*/

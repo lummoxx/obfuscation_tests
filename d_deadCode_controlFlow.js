913f31ac6fad05f8e7505bca44136c4dced168a6
lummoxx <52716403+lummoxx@users.noreply.github.com>
2023-05-10 10:39:34 +0200

    Create d_deadCode_controlFlow.js

---
+++ d_deadCode_controlFlow.js
@@ -0,0 +1,92 @@
+function tasma(mate, sakani) {
+  var luck = lariana();
+  tasma = function (kaiyen, tenesa) {
+    kaiyen = kaiyen - 500;
+    var lastarr = luck[kaiyen];
+    return lastarr;
+  };
+  return tasma(mate, sakani);
+}
+var armoney = tasma;
+(function (kimiah, titobiloluwa) {
+  var arielys = tasma;
+  var willibaldo = kimiah();
+  while (true) {
+    try {
+      var nanelle = parseInt(arielys(511)) / 1 * (-parseInt(arielys(520)) / 2) + -parseInt(arielys(515)) / 3 + parseInt(arielys(508)) / 4 * (parseInt(arielys(504)) / 5) + -parseInt(arielys(505)) / 6 + -parseInt(arielys(517)) / 7 * (-parseInt(arielys(512)) / 8) + -parseInt(arielys(506)) / 9 + -parseInt(arielys(518)) / 10 * (-parseInt(arielys(502)) / 11);
+      if (nanelle === titobiloluwa) {
+        break;
+      } else {
+        willibaldo.push(willibaldo.shift());
+      }
+    } catch (rylo) {
+      willibaldo.push(willibaldo.shift());
+    }
+  }
+}(lariana, 256156));
+var a = 5;
+var b = 10;
+var c = armoney(521);
+function add(earlin, kiziah) {
+  var mikkayla = armoney;
+  var kippi = {kgoiM: function (jaze, quamon) {
+    return jaze + quamon;
+  }};
+  return kippi[mikkayla(501)](earlin, kiziah);
+}
+function multiply(rodericus, ardilia) {
+  var noel = armoney;
+  var leiby = {MEMoR: function (marwa, gimena) {
+    return marwa * gimena;
+  }};
+  return leiby[noel(509)](rodericus, ardilia);
+}
+function divide(carin, fayrouz) {
+  var ulrica = armoney;
+  var patrena = {vBqgY: function (trinnie, pomposa) {
+    return trinnie / pomposa;
+  }};
+  return patrena[ulrica(516)](carin, fayrouz);
+}
+function greet(aevyn) {
+  var sonali = armoney;
+  var nyava = {mwrLn: function (cecelia, sadiel) {
+    return cecelia + sadiel;
+  }, yjmBd: "Hello, "};
+  console.log(nyava[sonali(514)](nyava[sonali(514)](nyava[sonali(522)], aevyn), "!"));
+}
+if (a > b) {
+  console[armoney(519)](armoney(507));
+} else if (a < b) {
+  console.log(armoney(513));
+} else {
+  console[armoney(519)](armoney(503));
+}
+for (var i = 0; i < 10; i++) {
+  console.log(i);
+}
+function lariana() {
+  var kaique = ["a is equal to b", "155cfOKEa", "362538FZtfQs", "547452TwbSAl", "a is greater than b", "27848THrELx", "MEMoR", "c is neither a string nor a number", "75908JuBLLy", "2036568Tltlhb", "a is less than b", "mwrLn", "1335768Rgsyct", "vBqgY", "14rPahYr", "356450QwxDuq", "log", "4SywJkn", "string", "yjmBd", "c is a string", "c is a number", "kgoiM", "77ecRjql"];
+  lariana = function () {
+    return kaique;
+  };
+  return lariana();
+}
+console.log(3);
+var myObject = {name: "John", age: 30, city: "New York"};
+console[armoney(519)](myObject.name);
+var x = 0;
+while (x < 5) {
+  console[armoney(519)](x);
+  x++;
+}
+switch (c) {
+  case "string":
+    console.log(armoney(523));
+    break;
+  case "number":
+    console.log(armoney(500));
+    break;
+  default:
+    console.log(armoney(510));
+}

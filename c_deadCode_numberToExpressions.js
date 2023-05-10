1d1711c1dd72aa6b2eac0cf38c7f3695cec3f4bd
lummoxx <52716403+lummoxx@users.noreply.github.com>
2023-05-10 11:50:32 +0200

    Create c_deadCode_numberToExpressions.js

---
+++ c_deadCode_numberToExpressions.js
@@ -0,0 +1,34 @@
+/*
+The obfuscated code is difficult to read and understand. It is obfuscated to hide its functionality. 
+However, I can explain what it does.
+
+The code defines several functions, including add, multiply, divide, and greet. It also defines three variables a, b, and c.
+
+The if statement checks if the value of a is greater than b. If true, it logs "a is greater than b" to the console. 
+If false, it checks if a is less than b and logs "a is less than b" if true. Otherwise, it logs "a is equal to b".
+
+The for loop logs the value of the variable i to the console as it iterates from -8523 to 890.
+
+The myArray variable is an array that contains four elements: 1, 2, 3, and 4.
+
+Here is the deobfuscated code:
+*/
+
+var _0x270ea1 = _0x39ad;
+
+function _0x39ad(_0x17805b, _0xca39a9) {
+    var _0x97ec73 = _0x54ea();
+    _0x39ad = function (_0x1bf470, _0x1be7fa) {
+        _0x1bf470 = _0x1bf470 - 1;
+        var _0xdaf5e0 = _0x97ec73[_0x1bf470];
+        return _0xdaf5e0;
+    };
+    return _0x39ad(_0x17805b, _0xca39a9);
+}
+
+(function (_0xf8cd9c, _0x2f07a8) {
+    var _0x530295 = _0x39ad;
+    var _0x58db16 = _0xf8cd9c();
+    while (!![]) {
+        try {
+            var _0x5cb69d = parseInt(_0x530295(0xd2)) / (-0x99 * 0x29 + 0x29 * 0x2f + 0x10fb) * (-parseInt(_0x530295(0xd7)) / (0x1cf1 + -0x16c7 + -0x628 * 0x1)) + -parseInt(_0x530295(0xdf)) / (-0x5b * 0x5a + 0x1 * -0xb95 + 0xe * 0x31d) + -parseInt(_0x530295(0xd0)) / (-0x840 + -0x2 * 0xc95 + 0x2 * 0x10b7) + parseInt(_0x530295(0xdc)) / (0x1af5 + -0x1 * 0x20b4 + -0x3ba4) * (parseInt(_0x530295(0xd4)) / (0x15e8 + -0xf * 0x21a + -0x269 * 0x4)) + parseInt(_0x530295(0xce)) / (-0x90b + 0x1014 + -0x45 * 0x1a) + -parseInt(_0x530295(0xcd)) / (-0x1dd6 + 0x1632 + 0x4 * 0x1eb) + parseInt(_0x530295(0xda)) / (-0x1c5 * 0x6 + 0xf26 + -0x1 * 0x19bb) * (parseInt(_0x530295(0xcb)) /
